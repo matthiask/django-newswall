8b4cf6a52108f0ab28b83372bf90999374f27dba
Matthias Kestenholz <mk@spinlock.ch>
2011-09-22 14:49:44 +0200

    Add simple views and templates

---
+++ newswall/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls.defaults import url, patterns, include
+
+from newswall import views
+
+
+urlpatterns = patterns('',
+    #url(r'^feed/$', StoryFeed()),
+    url(r'^$',
+        views.ArchiveIndexView.as_view(),
+        name='newswall_entry_archive'),
+    url(r'^(?P<year>\d{4})/$',
+        views.YearArchiveView.as_view(),
+        name='newswall_entry_archive_year'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',
+        views.MonthArchiveView.as_view(),
+        name='newswall_entry_archive_month'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
+        views.DayArchiveView.as_view(),
+        name='newswall_entry_archive_day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+        views.DateDetailView.as_view(),
+        name='newswall_entry_detail'),
+    url(r'^source/(?P<slug>[-\w]+)/$',
+        views.SourceArchiveIndexView.as_view(),
+        name='newswall_source_detail'),
+)
---
+++ newswall/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import get_object_or_404
+from django.views.generic import dates
+
+from newswall.models import Source, Story
+
+try:
+    from towel import paginator
+except ImportError:
+    from django.core import paginator
+
+
+__all__ = ('ArchiveIndexView', 'YearArchiveView', 'MonthArchiveView', 'DayArchiveView',
+    'DateDetailView', 'SourceArchiveIndexView')
+
+
+class NewswallMixin(object):
+    """
+    This mixin autodetects whether the blog is integrated through an
+    ApplicationContent and automatically switches to inheritance2.0
+    if that's the case.
+
+    Additionally, it adds the view instance to the template context
+    as ``view``.
+
+    This requires at least FeinCMS v1.5.
+    """
+
+    def get_context_data(self, **kwargs):
+        kwargs.update({'view': self})
+        return super(NewswallMixin, self).get_context_data(**kwargs)
+
+    def get_queryset(self):
+        return Story.objects.active() #.transform(entry_list_lookup_related)
+
+    def render_to_response(self, context, **response_kwargs):
+        if 'app_config' in getattr(self.request, '_feincms_extra_context', {}):
+            return self.get_template_names(), context
+
+        return super(NewswallMixin, self).render_to_response(
+            context, **response_kwargs)
+
+
+class ArchiveIndexView(NewswallMixin, dates.ArchiveIndexView):
+    paginator_class = paginator.Paginator
+    paginate_by = 20
+    date_field = 'timestamp'
+    template_name_suffix = '_archive'
+    allow_empty = True
+
+
+class YearArchiveView(NewswallMixin, dates.YearArchiveView):
+    paginator_class = paginator.Paginator
+    paginate_by = 20
+    date_field = 'timestamp'
+    make_object_list = True
+    template_name_suffix = '_archive'
+
+
+class MonthArchiveView(NewswallMixin, dates.MonthArchiveView):
+    paginator_class = paginator.Paginator
+    paginate_by = 20
+    month_format = '%m'
+    date_field = 'timestamp'
+    template_name_suffix = '_archive'
+
+
+class DayArchiveView(NewswallMixin, dates.DayArchiveView):
+    paginator_class = paginator.Paginator
+    paginate_by = 20
+    month_format = '%m'
+    date_field = 'timestamp'
+    template_name_suffix = '_archive'
+
+
+class DateDetailView(NewswallMixin, dates.DateDetailView):
+    paginator_class = paginator.Paginator
+    paginate_by = 20
+    month_format = '%m'
+    date_field = 'timestamp'
+
+    def get_queryset(self):
+        return Story.objects.active()
+
+
+class SourceArchiveIndexView(ArchiveIndexView):
+    template_name_suffix = '_archive'
+
+    def get_queryset(self):
+        self.source = get_object_or_404(Source, slug=self.kwargs['slug'])
+
+        queryset = super(SourceArchiveIndexView, self).get_queryset()
+        return queryset.filter(source=self.source)
+
+    def get_context_data(self, **kwargs):
+        return super(SourceArchiveIndexView, self).get_context_data(
+            source=self.source,
+            **kwargs)
