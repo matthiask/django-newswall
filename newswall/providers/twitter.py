06815818e6dd55451f2646f5fcd04b7546b77e06
Matthias Kestenholz <mk@spinlock.ch>
2011-09-22 16:18:51 +0200

    Add a specialized twitter provider not adding any `body` data

---
+++ newswall/providers/twitter.py
@@ -0,0 +1,27 @@
+"""
+Twitter RSS Feed Provider
+=========================
+
+Required configuration keys::
+
+    {
+    "provider": "newswall.providers.twitter",
+    "user": "feinheit"
+    }
+"""
+from datetime import datetime
+import feedparser
+import time
+
+from newswall.providers.base import ProviderBase
+
+
+class Provider(ProviderBase):
+    def update(self):
+        feed = feedparser.parse("http://twitter.com/statuses/user_timeline/%s.rss?count=100" % self.config['user'])
+
+        for entry in feed['entries']:
+            self.create_story(entry.link,
+                title=entry.title,
+                timestamp=datetime.fromtimestamp(time.mktime(entry.date_parsed)),
+                )
