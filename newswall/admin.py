a2920b9bf5386b3f92a8e2cd5f7c4251439b2c42
Matthias Kestenholz <mk@spinlock.ch>
2011-09-22 14:31:51 +0200

    Make a few fields editable from the changelist

--- newswall/admin.py
+++ newswall/admin.py
@@ -5,6 +5,7 @@ from newswall.models import Source, Story
 
 admin.site.register(Source,
     list_display=('name', 'is_active', 'ordering'),
+    list_editable=('is_active', 'ordering'),
     list_filter=('is_active',),
     prepopulated_fields={'slug': ('name',)},
     )
@@ -12,6 +13,7 @@ admin.site.register(Source,
 admin.site.register(Story,
     date_hierarchy='timestamp',
     list_display=('title', 'source', 'is_active', 'timestamp'),
+    list_editable=('is_active',),
     list_filter=('source', 'is_active'),
     search_fields=('object_url', 'title', 'author', 'content'),
     )
