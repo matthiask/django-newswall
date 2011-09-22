47b1019ce8f8cfa3f29052993b6ae26c46c0ee6d
Matthias Kestenholz <mk@spinlock.ch>
2011-09-22 14:56:39 +0200

    Add a few useful template tags

---
+++ newswall/templatetags/newswall_tags.py
@@ -0,0 +1,16 @@
+from django import template
+
+from newswall.models import Source, Story
+
+
+register = template.Library()
+
+
+@register.assignment_tag
+def newswall_sources():
+    return Source.objects.filter(is_active=True)
+
+
+@register.assignment_tag
+def newswall_archive_months():
+    return Story.objects.active().dates('timestamp', 'month', 'DESC')
