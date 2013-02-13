ad1c8492c7b83225eea93d8ec0aa98f2fdd7d446
Andrin Heusser <ah@feinheit.ch>
2013-02-13 13:55:47 +0100

    update the built-in twitter provider to use the api instead of pulling in a rss feed

--- newswall/providers/twitter.py
+++ newswall/providers/twitter.py
@@ -1,34 +1,60 @@
 """
-Twitter RSS Feed Provider
+Twitter API Feed Provider
 =========================
+
+---
+
+Required: tweepy
+
+pip install tweepy
+
+---
+
+Usage:
+
+Create a twitter app.
+You'll find the consumer_key/secret on the detail page.
+Because this is a read-only application, you can create
+your oauth_token/secret directly on the bottom of the app detail page.
+
+---
 
 Required configuration keys::
 
     {
     "provider": "newswall.providers.twitter",
-    "user": "feinheit"
+    "user": "feinheit",
+    "consumer_key": "...",
+    "consumer_secret": "...",
+    "oauth_token": "...",
+    "oauth_secret": "..."
     }
+
 """
-from datetime import datetime
-import feedparser
-import time
+import tweepy
 
 from newswall.providers.base import ProviderBase
 
 
 class Provider(ProviderBase):
     def update(self):
-        feed = feedparser.parse("http://twitter.com/statuses/user_timeline/%s.rss?count=100" % self.config['user'])
 
-        for entry in feed['entries']:
-            if hasattr(entry, 'date_parsed'):
-                timestamp = datetime.fromtimestamp(time.mktime(entry.date_parsed))
-            elif hasattr(entry, 'published_parsed'):
-                timestamp = datetime.fromtimestamp(time.mktime(entry.published_parsed))
-            else:
-                timestamp = datetime.now()
+        auth = tweepy.OAuthHandler(
+            self.config['consumer_key'],
+            self.config['consumer_secret']
+        )
+        auth.set_access_token(
+            self.config['oauth_token'],
+            self.config['oauth_secret']
+        )
 
-            self.create_story(entry.link,
-                title=entry.title,
-                timestamp=timestamp,
-                )
+        api = tweepy.API(auth)
+        entries = api.user_timeline(screen_name=self.config['user'])
+
+        for entry in entries:
+            link = 'http://twitter.com/%s/status/%s' % (self.config['user'],
+                entry.id)
+            self.create_story(link,
+                title=entry.text,
+                timestamp=entry.created_at
+            )
